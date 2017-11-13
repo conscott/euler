e025f0bc47007af28c5efb9f66017eb987f3c006
Conor Scott <conor.scott@clevertech.biz>
2017-11-13 14:09:00 +0400

    Add problems 1 and 2

---
+++ problem2.py
@@ -0,0 +1,14 @@
+#
+# By considering the terms in the Fibonacci sequence whose
+# values do not exceed four million, find the sum of the even-valued terms.
+#
+
+fib_seq = [1, 2]
+total = 2
+while (fib_seq[-1] < 4000000):
+    fib_seq.append(sum(fib_seq))
+    fib_seq.pop(0)
+    if fib_seq[-1] % 2 == 0:
+        total = total + fib_seq[-1]
+
+print total
