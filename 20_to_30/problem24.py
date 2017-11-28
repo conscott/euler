503e30f634206081cb458d20f97ebc64e07fc110
Conor Scott <conor.scott@clevertech.biz>
2017-11-28 15:08:54 +0400

    Problem 24

---
+++ 20_to_30/problem24.py
@@ -0,0 +1,5 @@
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+from itertools import permutations
+
+print ''.join([str(i) for i in list(permutations([0,1,2,3,4,5,6,7,8,9], 10))[999999]])
