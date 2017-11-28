2c2ff8892a853488f6613daa631295bdf2d1d02b
Conor Scott <conor.scott@clevertech.biz>
2017-11-28 15:16:35 +0400

    Problem 25

---
+++ 20_to_30/problem25.py
@@ -0,0 +1,10 @@
+# What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+fibs = [1, 1]
+idx = 2
+
+while len(str(fibs[0])) < 1000:
+    fibs = fibs[0] + fibs[1], fibs[0]
+    idx += 1
+
+print idx
