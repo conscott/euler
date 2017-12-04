b975d94a2fd7c787199bf27d089cc96b4c309dac
Conor Scott <conor.scott@clevertech.biz>
2017-12-04 17:56:11 +0400

    Problem 30

---
+++ 30_to_39/problem30.py
@@ -0,0 +1,7 @@
+# Find the sum of all the numbers that can be written as the sum of
+# fifth powers of their digits.
+
+def pow_digits(n, p):
+    return sum([int(d)**p for d in str(n)])
+
+print sum([i for i in range(2, 9**5*6) if pow_digits(i, 5) == i])
