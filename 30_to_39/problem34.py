2a30f8596c588973f4626a941487f10e27fb0215
Conor Scott <conor.r.scott.88@gmail.com>
2017-12-05 16:04:19 +0400

    Problem 34

---
+++ 30_to_39/problem34.py
@@ -0,0 +1,14 @@
+# Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+def fact(n):
+    if n == 1 or n == 0:
+        return 1
+    return n * fact(n-1)
+
+# Only going to need factorial of 0-9
+fact_cache = {i: fact(i) for i in range(10)}
+
+def fact_digits(num):
+    return sum([fact_cache[int(i)] for i in str(num)])
+
+print sum([i for i in range(3, 1000000) if fact_digits(i) == i])
